6e31c09269bfaafc646c804978ac8713b852b631
ravindrapv <ravindra.ravi4849@gmail.com>
2022-09-07 10:22:54 +0530

    flash message

---
+++ block-BNaadt/RegisterLogin/model/UserDB.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
+
+var userSchema = new Schema(
+  {
+    name: { type: String, requerd: true },
+    email: { type: String, requerd: true },
+    password: { type: String, minlength: 5, maxlength: 8, required: true },
+    phone:Number,
+    age:Number
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  if (this.password && this.isModified("password")) {
+    bcrypt.hash(this.password, 10, (err, hashed) => {
+      if (err) return next(err);
+      this.password = hashed;
+     next();
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.verfiypassword = function(password, cb){
+  bcrypt.compare(password, this.password, (err,result)=>{
+    return cb(err, result);
+  })
+}
+module.exports = mongoose.model("User", userSchema);
+
